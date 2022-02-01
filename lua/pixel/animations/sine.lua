13ce57240a8e657cec6d30d9b10d32d6d18126f6
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-01 13:49:33 +0100

    updated config

--- lua/pixel/animations/sine.lua
+++ lua/pixel/animations/sine.lua
@@ -1,17 +1,17 @@
 local pixel = require 'pixel'
 local color = require 'pixel.color'
-local divisor = 50
 local offset = 0
 local increment = 0.01
 
 return function()
-    local center = { math.floor((pixel.rows() + 1) / 2), math.floor((pixel.cols() + 1) / 2) }
     offset = offset + increment
     offset = offset - math.floor(offset)
     for r = 1, pixel.rows() do
+        local y = (r - 1) / (pixel.rows() - 1) * 2 - 1
+        local yy = y * y
         for c = 1, pixel.cols() do
-            local x, y = r - center[1], c - center[2]
-            local hue = (math.sqrt(x * x + y * y) / divisor)
+            local x = (c - 1) / (pixel.cols() - 1) * 2 - 1
+            local hue = (math.sqrt(x * x + yy))
             hue = hue + offset
             hue = hue - math.floor(hue)
             local red, green, blue = color.hsv_to_rgb(hue, 1, 1)
--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -1,5 +1,6 @@
 return {
     config = function()
+        vim.o.termguicolors = true
         nnoremap('<leader>sp', ':lua require"pixel".toggle()<cr>', 'silent', 'Pixel: switch on and off pixel display')
         local mul = 4
         require('pixel').setup { rows = 9 * mul, cols = 16 * mul, framerate = 10 }
