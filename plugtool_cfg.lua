7bdeed6d8fbd77b5851ae3a179c0ac0076990e1f
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-07 15:15:04 +0100

    reverted change to plugtool_cfg

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -4,6 +4,6 @@ return {
         nnoremap('<leader>sp', ':lua require"pixel".toggle()<cr>', 'silent', 'Pixel: switch on and off pixel display')
         local mul = 6
         require('pixel').setup { rows = 9 * mul + bit.band(mul, 1), cols = 16 * mul, framerate = 25 }
-        require('pixel').set_animation(require 'pixel.animations.portrend')
+        require('pixel').set_animation(require 'pixel.animations.cycle')
     end,
 }
