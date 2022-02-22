cc25f1d8b5f525cd59d24cffcca5a6e21e463527
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-22 18:40:52 +0100

    x

--- lua/pixel/render/engine/kitty.lua
+++ lua/pixel/render/engine/kitty.lua
@@ -2,49 +2,6 @@ local kitty = {}
 local string = require 'toolshed.util.string'
 local terminal = require 'pixel.render.terminal'
 
-local function chunks(data, size)
-    size = math.floor(math.abs(size or 512))
-    assert(size > 0, 'size cannot be negative')
-    local len = data:len()
-    local blocks = math.floor((len + size - 1) / size)
-    local ret = {}
-    for i = 1, blocks do
-        local start = size * (i - 1)
-        ret[i] = data:sub(start + 1, math.min(len, start + size))
-    end
-    return ret
-end
-
-local function send_cmd(cmd, data)
-    terminal.begin_transaction()
-    cmd, data = cmd or '', chunks(string.base64_encode(data or ''))
-    local num_chunks = #data
-    if cmd == '' or num_chunks <= 1 then
-        local esc = { '\x1b_G', cmd, ';' }
-        if num_chunks == 1 then
-            table.insert(esc, data[1])
-        end
-        table.insert(esc, '\x1b\\')
-        terminal.write(table.concat(esc))
-    else
-        for i = 1, num_chunks do
-            local esc = { '\x1b_G' }
-            if i == 1 then
-                table.insert(esc, cmd)
-                if cmd ~= '' then
-                    table.insert(esc, ',')
-                end
-            end
-            table.insert(esc, 'm=')
-            table.insert(esc, i == num_chunks and '0;' or '1;')
-            table.insert(esc, data[i])
-            table.insert(esc, '\x1b\\')
-            terminal.write(table.concat(esc))
-        end
-    end
-    terminal.end_transaction()
-end
-
 kitty.constants = {
     control_keys = {
         format = 'f',
@@ -78,11 +35,63 @@ kitty.constants = {
     action = { query = 'q', delete = 'd', transmit = 't', transmit_and_display = 'T' },
     quiet = { ok = '1', errors = '2' },
 }
+local valid_keys = {}
+for _, v in pairs(kitty.constants.control_keys) do
+    valid_keys[v] = true
+end
 
-terminal.execute_at(10, 100, function()
-    send_cmd('a=T,f=100,q=2,i=1,p=1', require('pixel.util').read_file '/home/cedric/dice.png')
-end)
-vim.defer_fn(function()
-    send_cmd 'a=d'
-end, 1000)
+local function chunks(data, size)
+    size = math.floor(math.abs(size or 4096))
+    assert(size > 0, 'size cannot be negative')
+    local len = data:len()
+    local blocks = math.floor((len + size - 1) / size)
+    local ret = {}
+    for i = 1, blocks do
+        local start = size * (i - 1)
+        ret[i] = data:sub(start + 1, math.min(len, start + size))
+    end
+    return ret
+end
+
+local function build_cmd(cmd)
+    local ret = {}
+    for k, v in pairs(cmd) do
+        if valid_keys[k] then
+            table.insert(ret, k .. '=' .. tostring(v))
+        end
+    end
+    return table.concat(ret, ',')
+end
+
+function kitty.send_cmd(cmd, data)
+    cmd = build_cmd(cmd)
+    terminal.begin_transaction()
+    cmd, data = cmd or '', chunks(string.base64_encode(data or ''))
+    local num_chunks = #data
+    if cmd == '' or num_chunks <= 1 then
+        local esc = { '\x1b_G', cmd, ';' }
+        if num_chunks == 1 then
+            table.insert(esc, data[1])
+        end
+        table.insert(esc, '\x1b\\')
+        terminal.write(table.concat(esc))
+    else
+        for i = 1, num_chunks do
+            local esc = { '\x1b_G' }
+            if i == 1 then
+                table.insert(esc, cmd)
+                if cmd ~= '' then
+                    table.insert(esc, ',')
+                end
+            end
+            table.insert(esc, 'm=')
+            table.insert(esc, i == num_chunks and '0;' or '1;')
+            table.insert(esc, data[i])
+            table.insert(esc, '\x1b\\')
+            terminal.write(table.concat(esc))
+        end
+    end
+    terminal.end_transaction()
+end
+
 return kitty
--- lua/pixel/render/image/init.lua
+++ lua/pixel/render/image/init.lua
@@ -1,4 +1,9 @@
 local image = {}
+local util = require 'pixel.util'
+local kitty = require 'pixel.render.engine.kitty'
+
+local img_id = 0
+
 local MT = {
     __index = image,
     __metatable = function() end,
@@ -13,14 +18,38 @@ local defaults = {
 function image.new(opts)
     opts = opts == nil and {} or opts
     opts = vim.tbl_deep_extend('force', defaults, opts)
-    print(vim.inspect(opts))
-    local ret = setmetatable({}, MT)
     if not opts.src then
-        error 'source not provided'
+        error 'src not provided'
     end
+    img_id = img_id + 1
+    local ret = setmetatable({
+        id = img_id,
+        placements = {},
+    }, MT)
+    local data = util.read_file(opts.src)
+    kitty.send_cmd({
+        a = 't',
+        t = 'd',
+        f = 100,
+        i = ret.id,
+        q = 2,
+    }, data)
     return ret
 end
 
-image.new { src = '/home/cedric/dice.png' }
+function image:display()
+    kitty.send_cmd { a = 'p', i = self.id, z = 1, C = 1 }
+end
 
+function image:destroy()
+    kitty.send_cmd { a = 'd', i = self.id }
+end
+
+local img = image.new { src = '/home/cedric/dice.png' }
+vim.defer_fn(function()
+    img:display()
+end, 1000)
+vim.defer_fn(function()
+    img:destroy()
+end, 2000)
 return image
