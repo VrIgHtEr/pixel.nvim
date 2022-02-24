0e1b9e2de4e36541e2697301f522d2f55a57d1f9
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-24 13:49:41 +0100

    updated mapping description

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -3,7 +3,7 @@ return {
         vim.o.termguicolors = true
         nnoremap('<leader>sp', ':lua require"pixel".toggle()<cr>', 'silent', 'Pixel: switch on and off pixel display')
         nnoremap('<leader>sP', ':lua require"pixel".toggle_colors()<cr>', 'silent', 'Pixel: toggle colors')
-        nnoremap('<leader>mario', ':lua require"pixel.render.image".its_a_meee()<cr>', 'silent', 'Pixel: toggle colors')
+        nnoremap('<leader>mario', ':lua require"pixel.render.image".its_a_meee()<cr>', 'silent', "It's-a meee! Mario!!!")
         local mul = 6
         require('pixel').setup { rows = 9 * mul + bit.band(mul, 1), cols = 16 * mul, framerate = 25 }
         require('pixel').set_animation(require 'pixel.animations.cycle')
