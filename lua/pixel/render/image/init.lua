d1890644fcdc753f70874f2fff39a0fe3c8a38e5
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-24 08:24:54 +0100

    mario

--- lua/pixel/render/image/init.lua
+++ lua/pixel/render/image/init.lua
@@ -15,6 +15,21 @@ local defaults = {
     format = format.png,
 }
 
+local offset_x, offset_y = 8, 4
+local width_x, width_y = 40, 40
+
+local data_path = vim.fn.stdpath 'data' .. '/site/pack/vrighter/opt/pixel.nvim/data'
+local img
+
+local counter = 30
+local sprite_x, sprite_y = 0, 0
+local anim_delay = 100
+
+local terminal = require 'pixel.render.terminal'
+
+local rows = vim.api.nvim_get_option 'lines'
+local cols = vim.api.nvim_get_option 'columns'
+
 function image.new(opts)
     opts = opts == nil and {} or opts
     opts = vim.tbl_deep_extend('force', defaults, opts)
@@ -37,19 +52,39 @@ function image.new(opts)
     return ret
 end
 
-function image:display()
-    kitty.send_cmd { a = 'p', i = self.id, z = 1, C = 1 }
+function image:display(opts)
+    local cmd = { a = 'p', i = self.id, z = 1, C = 1 }
+    opts = opts or {}
+    cmd = vim.tbl_deep_extend('keep', cmd, opts)
+    kitty.send_cmd(cmd)
 end
 
 function image:destroy()
     kitty.send_cmd { a = 'd', i = self.id }
 end
 
-local img = image.new { src = '/home/cedric/dice.png' }
-vim.defer_fn(function()
-    img:display()
-end, 1000)
-vim.defer_fn(function()
-    img:destroy()
-end, 2000)
+local function display_next()
+    terminal.execute_at(rows - 2, 0, function()
+        img:display {
+            x = sprite_x * width_x + offset_x,
+            y = sprite_y * width_y + offset_y,
+            w = width_x,
+            h = width_y,
+            q = 2,
+            p = 1,
+        }
+    end)
+    sprite_x = sprite_x + 1
+    sprite_x = sprite_x == 3 and 0 or sprite_x
+    counter = counter - 1
+    if counter > 0 then
+        vim.defer_fn(display_next, anim_delay)
+    else
+        image:destroy()
+    end
+end
+vim.schedule(function()
+    img = image.new { src = data_path .. '/mario.png' }
+    display_next()
+end)
 return image
