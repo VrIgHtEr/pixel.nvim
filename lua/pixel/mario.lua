9c0381175f46813e6a8225b69e6b2ea029077025
Cedric Mamo <vrighter@gmail.com>
2022-03-02 00:28:17 +0100

    refactor

--- lua/pixel/mario.lua
+++ lua/pixel/mario.lua
@@ -1,15 +1,33 @@
 local mario = {}
-local image = require 'pixel.render.image'
-local img = image.new { src = vim.fn.stdpath 'data' .. '/site/pack/vrighter/opt/pixel.nvim/data/mario.png' }
-img.size = { x = 96, y = 128 }
-img:transmit()
-local anim_delay = 40
-local started = false
+local image, terminal = require 'pixel.render.image', require 'pixel.render.terminal'
 
 local sprite_w, sprite_h = 32, 32
+local anim_delay = 40
+local frame_change_max = 4
 
 local characters = {}
-local frame_change_max = 4
+do
+    local img = image.new { src = vim.fn.stdpath 'data' .. '/site/pack/vrighter/opt/pixel.nvim/data/mario.png' }
+    img.size = { x = 96, y = 128 }
+    img:transmit()
+    for i = 1, math.floor(img.size.y / (sprite_h * 2)) do
+        characters[i] = {
+            z = -i,
+            p = i,
+            placement = img:create_placement(),
+            state = 'idle',
+            xpos = 0,
+            xinc = 0,
+            dir = math.random(0, 1) == 0,
+            sprite_x_dir = 1,
+            sprite_x = 0,
+            frame_change_counter = 0,
+        }
+    end
+end
+
+local started = false
+local stopping = false
 
 local function next_state(c)
     if c.state == 'animating' then
@@ -57,45 +75,41 @@ local function next_state(c)
     end
 end
 
-for i = 1, math.floor(img.size.y / (sprite_h * 2)) do
-    characters[i] = {
-        z = -i,
-        p = i,
-        placement = img:create_placement(),
-        state = 'idle',
-        xpos = 0,
-        xinc = 0,
-        dir = math.random(0, 1) == 0,
-        sprite_x_dir = 1,
-        sprite_x = 0,
-        frame_change_counter = 0,
-    }
-end
-
 function mario.its_a_meee()
     vim.schedule(mario.lets_a_gooo)
 end
 
-local terminal = require 'pixel.render.terminal'
 local function display_next()
     if started then
-        terminal.begin_transaction()
-        for _, char in ipairs(characters) do
-            next_state(char)
+        if stopping then
+            started = false
+            stopping = false
+        else
+            terminal.begin_transaction()
+            for _, char in ipairs(characters) do
+                next_state(char)
+            end
+            terminal.end_transaction()
+            vim.defer_fn(display_next, anim_delay)
         end
-        terminal.end_transaction()
-        vim.defer_fn(display_next, anim_delay)
     end
 end
 
 function mario.lets_a_gooo()
-    if not started then
+    if not started and not stopping then
         started = true
+        stopping = false
         image.discover_win_size(function()
             vim.schedule(function()
                 display_next()
             end)
         end)
+    end
+end
+
+function mario.oh_no()
+    if not started and not stopping then
+        stopping = true
     end
 end
 
